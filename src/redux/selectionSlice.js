7a111291481ce565e52e94714b38d0aa2cfde184
Eric Diaz <ericdiazeuka@gmail.com>
2025-07-30 18:51:05 -0300

    feat: links tree view added

--- src/redux/selectionSlice.js
+++ src/redux/selectionSlice.js
@@ -1,1 +1,42 @@
-//Your code here
+import { createSlice } from '@reduxjs/toolkit';
+
+const selectionSlice = createSlice({
+    name: 'selection',
+    initialState: {
+        selectedUrls: [],
+        filters: {
+            applications: [],
+            clients: [],
+            templates: [],
+            siteEditions: []
+        }
+    },
+    reducers: {
+        setSelectedUrls: (state, action) => {
+            state.selectedUrls = action.payload;
+        },
+        clearSelection: (state) => {
+            state.selectedUrls = [];
+        },
+        setFilters: (state, action) => {
+            state.filters = { ...state.filters, ...action.payload };
+        },
+        clearFilters: (state) => {
+            state.filters = {
+                applications: [],
+                clients: [],
+                templates: [],
+                siteEditions: []
+            };
+        }
+    }
+});
+
+export const {
+    setSelectedUrls,
+    clearSelection,
+    setFilters,
+    clearFilters
+} = selectionSlice.actions;
+
+export default selectionSlice.reducer;
